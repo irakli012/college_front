a092e5c798f1d46629c911dbbffc8f1a63bacfa2
irakli012 <iraklimate@gmail.com>
2026-02-05 11:28:18 +0400

    performance improvment for rendering

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './i18n';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error("Could not find root element to mount to");
